f79ddcbae392309d78d552229f2711c2108a6aaf
Jonathan Lui <jonathanlui@google.com>
2018-07-17 10:39:39 -0700

    gen: add version v1p3beta1 to synth.py

--- synth.py
+++ synth.py
@@ -23,7 +23,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 gapic = gcp.GAPICGenerator()
 
-versions = ['v1', 'v1p1beta1', 'v1p2beta1']
+versions = ['v1', 'v1p1beta1', 'v1p2beta1', 'v1p3beta1']
 
 for version in versions:
     library = gapic.node_library('vision', version)
