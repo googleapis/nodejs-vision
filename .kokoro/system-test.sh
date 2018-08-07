11dc8c8d56918d222c1fed884d0d470bcde2ca61
Jonathan Lui <jonathanlui@google.com>
2018-08-07 11:29:29 -0700

    build: add samples and system test cfg and .sh runner

--- .kokoro/system-test.sh
+++ .kokoro/system-test.sh
@@ -16,6 +16,9 @@
 
 set -xeo pipefail
 
+# Setup service account credentials.
+export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/service-account.json
+
 cd github/nodejs-vision
 
 npm install
