7658911a10f051f37ad7a04f25b0a4ec2c6628a9
Alix Hamilton <ajhamilton@google.com>
2018-08-14 16:30:38 -0700

    adds import tag to face detection tutorial

--- samples/faceDetection.js
+++ samples/faceDetection.js
@@ -15,13 +15,14 @@
 
 'use strict';
 
-// [START vision_face_detection_tutorial_client]
+// [START vision_face_detection_tutorial_imports]
 // By default, the client will authenticate using the service account file
 // specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
 // the project specified by the GCLOUD_PROJECT environment variable. See
 // https://googlecloudplatform.github.io/gcloud-node/#/docs/google-cloud/latest/guides/authentication
 var vision = require('@google-cloud/vision');
-
+// [END vision_face_detection_tutorial_imports]
+// [START vision_face_detection_tutorial_client]
 // Creates a client
 var client = new vision.ImageAnnotatorClient();
 
