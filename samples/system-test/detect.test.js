701a6973bba2dca73950909522afa0fc5e94f579
Noah Negrey <nnegrey@users.noreply.github.com>
2018-12-27 12:28:32 -0800

    Update detect.test.js

--- samples/system-test/detect.test.js
+++ samples/system-test/detect.test.js
@@ -251,8 +251,8 @@ describe(`detect`, () => {
   });
 
   it(`should extract text from pdf file`, async () => {
-    const output = await exec(`${cmd} pdf ${bucketName} ${files[7].name}`);
-    assert.match(output, `/${prefix}`);
+    const output = await exec(`${cmd} pdf ${bucketName} ${files[7].name} ${prefix}`);
+    assert.match(output, `/${prefix}/`);
   });
 
   it(`should detect objects in a local file`, async () => {
